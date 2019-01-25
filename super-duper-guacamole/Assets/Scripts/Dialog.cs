069321034ca1be493d3cae13cae17242428b93d5
Danny Martini <danny@wunderflats.com>
2019-01-25 23:52:20 +0100

    --wip--

---
+++ super-duper-guacamole/Assets/Scripts/Dialog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour
+{
+    public Dialogues npc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        npc.SetTree("Tenant 1");
+        Display();
+    }
+
+    public void Display()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
