92f5e52e8777b6b7777a6dc9637def9cf313e2ef
DESKTOP-5US3FU0\Niaz <tigrexspalterlp@gmail.com>
2019-01-27 00:24:34 +0100

    - restart implemented

---
+++ super-duper-guacamole/Assets/Scripts/ReplayScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReplayScript : MonoBehaviour
+{
+    public void restart() {
+        SceneManager.LoadScene("Home");
+    }
+}
