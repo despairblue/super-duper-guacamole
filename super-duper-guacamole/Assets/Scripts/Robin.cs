77b1d7c27f7bc07d6897db6fa3dcaa8b79960623
Danny Martini <danny@wunderflats.com>
2019-01-27 13:32:35 +0100

    add robin easteregg

---
+++ super-duper-guacamole/Assets/Scripts/Robin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robert : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var music = GameObject.Find("Audio Source");
+        if (music != null)
+        {
+            var audioSource = music.GetComponent<AudioSource>();
+            audioSource.Stop();
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
