297f9232afdbec569ed6ea0eac9276aed295d45a
MrGreenTea <mrlordalfred@gmail.com>
2019-01-27 14:18:55 +0100

    end scene scroll

---
+++ super-duper-guacamole/Assets/Scripts/CreditScroll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DigitalRuby.Tween;
+
+public class CreditScroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int max = 4000;
+    void Start()
+    {
+        System.Action<ITween<Vector2>> setPos = (t) =>
+            {
+                transform.localPosition = t.CurrentValue;
+            };
+
+        TweenFactory.Tween(
+            "Scroll",
+            transform.localPosition,
+            transform.localPosition + new Vector3(0, max, 0),
+            max/100,  // 100 units per second
+            TweenScaleFunctions.Linear,
+            setPos
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ super-duper-guacamole/Assets/Scripts/Wait.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wait : MonoBehaviour
+{
+    float phase = 0;
+    float y = -10e10f;
+
+    RectTransform[] dots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dots = GetComponentsInChildren<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        phase += Time.deltaTime * 8;
+        foreach (RectTransform dot in dots) {
+            dot.localPosition = new Vector2(
+                dot.localPosition.x,
+                Mathf.Sin(dot.localPosition.x/50 * Mathf.PI + phase) * 10 - 20
+            );
+        }
+    }
+}
